ef9398d394c006036675b4b160234657a6ac6806
mnnandev <mnnanhavibhutta@gamil.com>
2024-08-01 05:50:10 -0700

    code updated 8.1.2024

--- src/app/components/common/category/category.component.ts
+++ src/app/components/common/category/category.component.ts
@@ -1,95 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
-    selector: 'app-category',
-    templateUrl: './category.component.html',
-    styleUrls: ['./category.component.scss']
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.scss']
 })
-export class CategoryComponent implements OnInit {
+export class CategoryComponent implements AfterViewInit {
+  @ViewChild('carousel', { static: false }) carousel: ElementRef;
 
-    constructor() { }
+  profiles = [
+    { image: 'assets/img/user1.jpg', name: 'Add Story', isAddStory: true },
+    { image: 'assets/img/user1.jpg', name: 'Alex', isAddStory: false },
+    { image: 'assets/img/user1.jpg', name: 'Sam', isAddStory: false },
+    { image: 'assets/img/user1.jpg', name: 'Taylor', isAddStory: false },
+    { image: 'assets/img/user1.jpg', name: 'Taylor', isAddStory: false },
+    { image: 'assets/img/user1.jpg', name: 'Taylor', isAddStory: false },
+    { image: 'assets/img/user1.jpg', name: 'Taylor', isAddStory: false },
+    { image: 'assets/img/user1.jpg', name: 'Taylor', isAddStory: false },
+    { image: 'assets/img/user1.jpg', name: 'Taylor', isAddStory: false },
+    { image: 'assets/img/user1.jpg', name: 'Taylor', isAddStory: false },
+    { image: 'assets/img/user1.jpg', name: 'Taylor', isAddStory: false },
+    { image: 'assets/img/user1.jpg', name: 'Taylor', isAddStory: false },
+    { image: 'assets/img/user1.jpg', name: 'Taylor', isAddStory: false },
+    { image: 'assets/img/user1.jpg', name: 'Taylor', isAddStory: false },
+    // other profiles
+    { image: 'assets/img/user1.jpg', name: 'Jordan', isAddStory: false }
+  ];
 
-    ngOnInit(): void {
+  customOptions: OwlOptions = {
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 4
+      },
+      600: {
+        items: 5
+      },
+      1000: {
+        items: 4
+      }
     }
+  };
 
-    sectionTitle = [
-        {
-            title: 'Browse Businesses by Category',
-            paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra.'
-        }
-    ]
-    singleCategoryBox = [
-        {
-            icon: 'flaticon-cooking',
-            title: 'Restaurant',
-            numberOfPlaces: '16 Places',
-            link: 'grid-listings-left-sidebar'
-        },
-        {
-            icon: 'flaticon-hotel',
-            title: 'Hotel',
-            numberOfPlaces: '42 Places',
-            link: 'grid-listings-left-sidebar'
-        },
-        {
-            icon: 'flaticon-exercise',
-            title: 'Fitness',
-            numberOfPlaces: '11 Places',
-            link: 'grid-listings-left-sidebar'
-        },
-        {
-            icon: 'flaticon-commerce',
-            title: 'Shopping',
-            numberOfPlaces: '24 Places',
-            link: 'grid-listings-left-sidebar'
-        },
-        {
-            icon: 'flaticon-cleansing',
-            title: 'Beauty & Spa',
-            numberOfPlaces: '8 Places',
-            link: 'grid-listings-left-sidebar'
-        },
-        {
-            icon: 'flaticon-calendar',
-            title: 'Events',
-            numberOfPlaces: '12 Places',
-            link: 'grid-listings-left-sidebar'
-        },
-        {
-            icon: 'flaticon-heart-1',
-            title: 'Health Care',
-            numberOfPlaces: '16 Places',
-            link: 'grid-listings-left-sidebar'
-        },
-        {
-            icon: 'flaticon-airport',
-            title: 'Travel & Public',
-            numberOfPlaces: '8 Places',
-            link: 'grid-listings-left-sidebar'
-        },
-        {
-            icon: 'flaticon-car-insurance',
-            title: 'Auto Insurance',
-            numberOfPlaces: '10 Places',
-            link: 'grid-listings-left-sidebar'
-        },
-        {
-            icon: 'flaticon-attorney',
-            title: 'Attorneys',
-            numberOfPlaces: '25 Places',
-            link: 'grid-listings-left-sidebar'
-        },
-        {
-            icon: 'flaticon-plumber',
-            title: 'Plumbers',
-            numberOfPlaces: '5 Places',
-            link: 'grid-listings-left-sidebar'
-        },
-        {
-            icon: 'flaticon-more-1',
-            title: 'More Categories',
-            link: 'grid-listings-left-sidebar'
-        }
-    ]
+  constructor() { }
 
+  ngAfterViewInit() {
+    // Ensure the DOM is fully loaded before manipulating it
+    setTimeout(() => {
+      this.updateWidth('.owl-item', '68.8px');
+    }, 1000); // Adjust the delay as needed
+  }
+
+  updateWidth(selector: string, newWidth: string) {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(element => {
+      if (element instanceof HTMLElement) {
+        element.style.removeProperty('width');
+        element.style.width = newWidth;
+      }
+    });
+  }
 }
