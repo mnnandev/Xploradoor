ab8cf771b2ac36204ff460f1d79f0c7b4c2b621e
mnnandev <mnnanhavibhutta@gamil.com>
2024-08-08 03:10:14 -0700

    code updated 8.8.2024 with model

--- src/app/components/pages/dashboard/dashboard-add-blog/dashboard-add-blog.component.ts
+++ src/app/components/pages/dashboard/dashboard-add-blog/dashboard-add-blog.component.ts
@@ -19,5 +19,26 @@ export class DashboardAddBlogComponent implements OnInit {
             subTitle: 'Dashboard'
         }
     ]
+    images = [
+        { id: 1, src: 'assets/img/listings/listings4.jpg', alt: 'Image 1' },
+        { id: 2, src: 'assets/img/listings/listings4.jpg', alt: 'Image 2' },
+        { id: 3, src: 'assets/img/listings/listings4.jpg', alt: 'Image 3' },
+        // Add more images as needed
+      ];
+    
+      selectedImages: Set<number> = new Set();
+    
+      toggleSelection(imageId: number) {
+        if (this.selectedImages.has(imageId)) {
+          this.selectedImages.delete(imageId);
+        } else {
+          this.selectedImages.add(imageId);
+        }
+      }
+    
+      deleteSelectedImages() {
+        this.images = this.images.filter(image => !this.selectedImages.has(image.id));
+        this.selectedImages.clear();
+      }
 
 }
